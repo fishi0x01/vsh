0da7b8b806680582c71af93c7951782bdb532ebe
Karl Fischer <fishi0x01@gmail.com>
2019-10-08 01:22:33 +0200

    .gif and test adjustment

--- test/kv-sh.sh
+++ test/kv-sh.sh
@@ -8,7 +8,6 @@ export VAULT_TOKEN="root"
 export VAULT_VERSION="1.2.2"
 export VAULT_ADDR="http://localhost:${VAULT_PORT}"
 export VAULT_CONTAINER_NAME="vault"
-export VAULT_TEST_VALUE="test"
 
 { # Try
 start_vault ${VAULT_VERSION} ${VAULT_CONTAINER_NAME} ${VAULT_PORT}
@@ -17,23 +16,15 @@ start_vault ${VAULT_VERSION} ${VAULT_CONTAINER_NAME} ${VAULT_PORT}
 vault_exec ${VAULT_CONTAINER_NAME} "vault secrets disable secret"
 vault_exec ${VAULT_CONTAINER_NAME} "vault secrets enable -version=2 -path=secretkv2 kv"
 
-vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv2/source/a value=${VAULT_TEST_VALUE}"
-vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv2/source/b value=${VAULT_TEST_VALUE}"
-vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv2/source/c/d value=${VAULT_TEST_VALUE}"
-vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv2/source/c/e value=${VAULT_TEST_VALUE}"
-
-vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv2/remove/x value=${VAULT_TEST_VALUE}"
-vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv2/remove/y/z value=${VAULT_TEST_VALUE}"
+vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv2/departmentA/keyA value=secret-key-A"
+vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv2/departmentA/keyB value=secret-key-B"
+vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv2/departmentA/subkeys/keyC value=secret-key-C"
+vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv2/departmentA/subkeys/keyD value=secret-key-D"
 
 ## Setup v1 KV
 vault_exec ${VAULT_CONTAINER_NAME} "vault secrets enable -version=1 -path=secretkv1 kv"
 
-vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv1/source/a value=${VAULT_TEST_VALUE}"
-vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv1/source/b value=${VAULT_TEST_VALUE}"
-vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv1/source/c/d value=${VAULT_TEST_VALUE}"
-vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv1/source/c/e value=${VAULT_TEST_VALUE}"
-
-vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv1/remove/x value=${VAULT_TEST_VALUE}"
-vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv1/remove/y/z value=${VAULT_TEST_VALUE}"
-
+vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv1/departmentB/secretA value=secretA"
+vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv1/departmentB/secretB value=secretB"
+vault_exec ${VAULT_CONTAINER_NAME} "vault kv put secretkv1/departmentB/subsecret/secretC value=secretC"
 }
